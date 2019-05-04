0d1a227b95a4656890257818148fbbbcb48d2900
Topher Lee <topher.lee.13@gmail.com>
2019-05-04 16:07:34 -0400

    RomListButton
    Add Rom List Button Prefab
    Add RomListButton.cs

---
+++ Assets/Plugins/RetroUnity/Scripts/UI/ROMListButton.cs
@@ -0,0 +1,37 @@
+﻿using RetroUnity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AltX.UI
+{
+    public class ROMListButton : MonoBehaviour
+    {
+        public GameManager gameManager;
+        public string coreFileName;
+        public string coreName;
+        public string romFileName;
+        public string romName;
+
+        public Sprite platformIcon; // SNES.png
+        public Sprite platformMediaIcon; // SNES-Cartridge.png
+
+        public Image iconSlot;
+        public Text romNameSlot;
+
+        private void Start()
+        {
+            gameManager = GameObject.Find("Libretro").GetComponent<GameManager>();
+            romNameSlot.text = romName;
+            iconSlot.sprite = platformIcon;
+        }
+        public void OnPressed()
+        {
+            gameManager.CoreFileName = coreFileName;
+            gameManager.RomFileName = romFileName;
+            gameManager.LoadRom(gameManager.romPath + "/snes/" + gameManager.RomFileName);
+        }
+    }
+}
+
